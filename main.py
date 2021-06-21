10962e4b3241ba732db3e8138ae3aecb76c02701
Rahul-Raoniar <rahul.raoniar@outlook.com>
2021-06-21 16:42:36 +0530

    Fixed bugs 21 June 2021

--- main.py
+++ main.py
@@ -32,18 +32,11 @@ desc_temp = """
 
 			"""
 
-"""
-This function load csv data from streamlit app input
-"""
 
 def load_data():
     data_file = st.file_uploader("Upload a CSV", type=["csv"])
     return data_file
 
-
-"""
-Main function contains all the functions
-"""
 
 def main():
     stc.html(html_temp)
